658b33171684eb80c95d3b1463d706286dee0e32
MagicTurtle <raffertychen@gmail.com>
2019-04-27 06:29:20 -0700

    added debugging flag
    debug argument (if set to true) will show all faces detected (with selected face shown in blue and others shown in red) along with the confidence level for each face as well as print out some information about each detection such as its shape and distance from the center of the image

--- tools/visualize.py
+++ tools/visualize.py
@@ -13,13 +13,19 @@
 # when running from the command line, optional parameters are:
 #
 # -p/--path [path to image directory]
-## if not defined, the program will prompt user for path
+#   if not defined, the program will prompt user for path
 #
 # -s/--start [index of image directory]
-## allows user to pick up from where they left off after deleting an image
-## using ctrl-c will also show how many pictures were gone through in a session
-## so that the user can add that number to their previous start value
-## can be modified directly in the code, default is 0
+#   allows user to pick up from where they left off after deleting an image
+#   using ctrl-c will also show how many pictures were gone through in a session
+#   so that the user can add that number to their previous start value
+#   can be modified directly in the code, default is 0
+#
+# -d/--debug [True]
+#   will show all faces detected (with selected face shown in blue and others
+#   shown in red) along with the confidence level for each face as well as 
+#   print out some information about each detection such as its shape and distance
+#   from the center of the image
 
 import argparse
 import cv2
@@ -30,6 +36,9 @@ from pathlib import Path
 
 # set start here
 START_FROM = 0
+
+# det debug here
+DEBUG = False
 
 # set amount of time each picture is shown for (in ms)
 WAIT_TIME = 300
@@ -59,7 +68,7 @@ def distance_x(center_1: tuple, center_2: tuple) -> float:
     return abs(center_1[0] - center_2[0])
 
 
-def detect_face(image) -> tuple:
+def detect_face(image, debug: bool = False) -> tuple:
     height, width = image.shape[:2]
     blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
 
@@ -78,15 +87,9 @@ def detect_face(image) -> tuple:
             x, y, w, h = box.astype("int")
             
             detections_set.add((confidence, (x, y, w, h)))
-    
-    # DEBUGGING
 
-    for confidence, detection in detections_set:
-        print(f"center of image: {get_center((0, 0, width, height))}\n"
-            f"shape: {detection}\n"
-            f"distance from center y-axis: {distance_x(get_center(detection), get_center((0, 0, width, height)))}\n"
-            f"distance from center: {distance(get_center(detection), get_center((0, 0, width, height)))}\n"
-            f"confidence: {confidence}\n")
+    if debug:
+        return detections_set
     
     try:
         max_confidence = max(detections_set, key=lambda x: x[0])
@@ -99,12 +102,44 @@ def detect_face(image) -> tuple:
         else:
             output = min_distance[1]
         return output
+
+
+def debug_draw_rects(image, detections) -> "image":
+    height, width = image.shape[:2]
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    max_confidence = max(detections, key=lambda x: x[0])
+    min_distance = min(detections, key=lambda x: distance_x(get_center(x[1]), get_center((0, 0, width, height))))
+
+    if max_confidence[0] - 0.10 > min_distance[0]:
+        output = max_confidence[1]
+    else:
+        output = min_distance[1]
+
+    for confidence, detection in detections:
+        print(f"center of image: {get_center((0, 0, width, height))}\n"
+            f"detection shape: {detection}\n"
+            f"distance from center y-axis: {distance_x(get_center(detection), get_center((0, 0, width, height)))}\n"
+            f"confidence: {confidence}\n")
+
+        x, y, w, h = detection
+
+        if detection == output:
+            cv2.rectangle(image, (x, y), (w, h), (0, 0, 255), 2)
+            cv2.putText(image, f"{confidence*100:.2f}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.50, (0, 0, 255), 2)
+        else:
+            cv2.rectangle(image, (x, y), (w, h), (255, 0, 0), 2)
+            cv2.putText(image, f"{confidence*100:.2f}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.50, (255, 0, 0), 2)     
+    
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
     
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument("-p", "--path", help="path to image directory")
     ap.add_argument("-s", "--start", type=int, help="image number to start from")
+    ap.add_argument("-d", "--debug", type=bool, help="show all detections")
     args = ap.parse_args()
 
     if args.path:
@@ -136,18 +171,31 @@ if __name__ == '__main__':
                     img = cv2.resize(img, (SCREEN_WIDTH//5*2,
                                            int(SCREEN_WIDTH//5*2 * img.shape[0]/img.shape[1])))
 
-                coords = detect_face(img)
+                if DEBUG or args.debug:
+                    detections = detect_face(img, True)
 
-                if coords is not None:
-                    (x, y, w, h) = coords
+                    if detections:
+                        img = debug_draw_rects(img, detections)
                         
-                    cv2.rectangle(img, (x, y), (w, h), (255, 0, 0), 2)
-                    
-                    cv2.imshow(p.name, img)
-                    if cv2.waitKey(0) & 0xFF == ord("q"):
+                        cv2.imshow(p.name, img)
+                        if cv2.waitKey(WAIT_TIME) & 0xFF == ord("q"):
+                            cv2.destroyAllWindows()
+                            break
                         cv2.destroyAllWindows()
-                        break
-                    cv2.destroyAllWindows()
+
+                else:
+                    coords = detect_face(img)
+
+                    if coords is not None:
+                        (x, y, w, h) = coords
+                            
+                        cv2.rectangle(img, (x, y), (w, h), (255, 0, 0), 2)
+                        
+                        cv2.imshow(p.name, img)
+                        if cv2.waitKey(WAIT_TIME) & 0xFF == ord("q"):
+                            cv2.destroyAllWindows()
+                            break
+                        cv2.destroyAllWindows()
 
                 count += 1
 
